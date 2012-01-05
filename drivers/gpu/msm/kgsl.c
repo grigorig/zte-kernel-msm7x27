c16e32515e14d89117ad30d8eb4eb1d1e7570f5a
Michael Street <mstreet@codeaurora.org>
2012-01-05 14:55:01 -0800

    msm: kgsl: DRM now using new memory interfaces
    Modified to use memory management API provided by KGSL.  Cleaned up
    unused function overrides, and fixed memory leak.
    
    Change-Id: I28c2d5c56b559e0ee5c46d25a2a22d694d3b6b9d
    Signed-off-by: Michael Street <mstreet@codeaurora.org>
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_drm.c

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2204,6 +2204,7 @@ EXPORT_SYMBOL(kgsl_register_device);
 int kgsl_device_platform_probe(struct kgsl_device *device,
 			       irqreturn_t (*dev_isr) (int, void*))
 {
+	int result;
 	int status = -EINVAL;
 	struct kgsl_memregion *regspace = NULL;
 	struct resource *res;
@@ -2264,6 +2265,9 @@ int kgsl_device_platform_probe(struct kgsl_device *device,
 		device->id, regspace->mmio_phys_base,
 		regspace->sizebytes, regspace->mmio_virt_base);
 
+	result = kgsl_drm_init(pdev);
+	if (result)
+		goto error_iounmap;
 
 	status = kgsl_register_device(device);
 	if (!status)
@@ -2398,11 +2402,6 @@ static int __init kgsl_core_init(void)
 		if (result)
 			goto err;
 	}
-
-	result = kgsl_drm_init(NULL);
-
-	if (result)
-		goto err;
 
 	return 0;
 
